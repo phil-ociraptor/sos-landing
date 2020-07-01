954f2c50ffe2669face17e50a0e5a74ed66f37d5
omri08 <54754198+omri08@users.noreply.github.com>
2020-07-01 17:48:59 +0300

    Added dataFetcher and dummy data

---
+++ pages/api/dataFetcher.js
@@ -0,0 +1,15 @@
+import { projects, people } from "./fakeData";
+
+export default class DataFetcher {
+  constructor(dbStrategy) {
+    this.strategy = dbStrategy;
+  }
+
+  async getAllProjects() {
+    return await this.strategy.getAllProjects();
+  }
+
+  async getAllPeople() {
+    return await this.strategy.getAllPeople();
+  }
+}
---
+++ pages/api/index.js
@@ -0,0 +1,4 @@
+import DataFetcher from "./dataFetcher";
+import FakeDbStrategy from "./strategies/fakeDbStrategy";
+
+export { DataFetcher, FakeDbStrategy };
