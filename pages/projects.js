38acb27983db4b97944323a9a1c1bce2a5759eee
Omar Moghazy <omar.moghazy.42@gmail.com>
2020-07-13 21:55:46 +0200

    Set up mobx and add /projects page  (#35)
    * set up mobx
    
    * added dependencies to package.json
    
    * added more missing dependencies to package.json

---
+++ pages/projects.js
@@ -0,0 +1,15 @@
+import { createContext } from 'react'
+import stores from "../stores/"
+import ProjectsList from "../components/ProjectsList"
+
+export const StoreContext = createContext();
+export const StoreProvider = StoreContext.Provider;
+
+export default function ProjectsPage() {
+    return ( 
+    <StoreProvider value={stores}>
+        <h1>Projects List:</h1>
+        <ProjectsList />
+    </StoreProvider>
+    )
+}
