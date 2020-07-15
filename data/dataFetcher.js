bb2744122d09319c7bedaec94a21fc004d65bac0
Omri malka <54754198+omri08@users.noreply.github.com>
2020-07-15 08:01:13 +0300

    Add data fetcher and dummy data  (#34)
    * Added dataFetcher and dummy data
    
    * Added dataFetcher and dummy data
    
    * revert commit from pages/index.js
    
    * another to push
    
    * moved data folder outside

---
+++ data/dataFetcher.js
@@ -0,0 +1,13 @@
+export default class DataFetcher {
+  constructor(dbStrategy) {
+    this.strategy = dbStrategy;
+  }
+
+  async getAllProjects() {
+    return await this.strategy.getAllProjects();
+  }
+
+  async getAllPeople() {
+    return await this.strategy.getAllPeople();
+  }
+}
---
+++ data/strategies/fakeDbStrategy.js
@@ -0,0 +1,21 @@
+import { projects, people } from "../fakeData";
+
+export default class FakeDbStrategy {
+  async getAllProjects() {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve(projects), 1000);
+    });
+
+    let result = await promise;
+    return result;
+  }
+
+  async getAllPeople() {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve(people), 1000);
+    });
+
+    let result = await promise;
+    return result;
+  }
+}
