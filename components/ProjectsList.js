fe0e3a61af2063d61bc5a5c08be1d1333f440b4f
pandevim <anirudh.pandev@gmail.com>
2020-08-14 13:27:45 +0530

    add apollo client to access hasura backend

--- components/ProjectsList.js
+++ components/ProjectsList.js
@@ -1,17 +1,37 @@
 import { useContext } from 'react'
-import { StoreContext } from '../pages/projects'
-import { useObserver } from 'mobx-react'
+import { gql, useQuery } from '@apollo/client'
+
+const GET_PROJECTS = gql`
+  query MyQuery {
+    project {
+      id
+      name
+      summary
+      description
+    }
+  }
+`
 
 export default function ProjectsList() {
-  const store = useContext(StoreContext)
+  const { data, loading, error, fetchMore } = useQuery(GET_PROJECTS)
 
-  return useObserver(() => (
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>ERROR</p>
+  if (!data) return <p>Not found</p>
+
+  return (
     <ol className="projects-list">
-      {store.ProjectsStore.projects.map((project) => (
-        <li key={project.name}>
-          <h4>{project.shortDesc}:</h4> <p>{project.longDesc}</p>
-        </li>
-      ))}
+      {data.project &&
+        data.project.map((project) => (
+          <li key={project.id}>
+            <h1>{project.name}</h1>
+            <b>{project.summary}</b>
+            <p>{project.description}</p>
+          </li>
+        ))}
+      {data.project.hasMore && (
+        <button onClick={() => fetchMore()}>Load More Projects</button>
+      )}
     </ol>
-  ))
+  )
 }
